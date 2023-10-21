a44ab54c2f566061bb52c56f258ec7b318c9827d
Brenda1Vargas <brendal.vargasp@konradlorenz.edu.co>
2023-10-21 09:55:04 -0500

    Agregar archivos de proyecto.

---
+++ LugaresFrecuentes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application
+{
+
+    public class LugarFrecuente
+    {
+        private string nombre;
+        private double latitud;
+        private double longitud;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public double Latitud
+        {
+            get { return latitud; }
+            set { latitud = value; }
+        }
+
+        public double Longitud
+        {
+            get { return longitud; }
+            set { longitud = value; }
+        }
+
+        public LugarFrecuente(string nombre, double latitud, double longitud)
+        {
+            Nombre = nombre;
+            Latitud = latitud;
+            Longitud = longitud;
+        }
+    }
+}
