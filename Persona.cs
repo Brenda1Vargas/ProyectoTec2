bb25e189513fe9f896a1341127b8dbad3e48f3e9
Vivi271 <142178270+Vivi271@users.noreply.github.com>
2023-11-04 09:08:23 -0500

    Act

--- Persona.cs
+++
@@ -1,7 +0,0 @@
-﻿public abstract class Persona
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-
-    public abstract void DetectarPeligro(string mensajePeligro);
-}
