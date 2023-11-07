8c5b5b3f92e06803f1949d20cab755814dc0a1a7
Vivi271 <142178270+Vivi271@users.noreply.github.com>
2023-11-06 20:32:10 -0500

    Actualizacion

--- Usuario.cs
+++ Usuario.cs
@@ -11,22 +11,16 @@ namespace Application
             get { return _id; }
             set { _id = value; }
         }
-
-        private string firstName;
         public string FirstName
         {
             get { return firstName; }
             set { firstName = value; }
         }
-
-        private string lastName;
         public string LastName
         {
             get { return lastName; }
             set { lastName = value; }
         }
-
-        private int age;
         public int Age
         {
             get { return age; }
